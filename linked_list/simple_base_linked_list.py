8cc67e025f8c3c9aed82aab0ee3c24479bc1b39c
Naga Puneeth Devatha <devatha.naga_puneeth@nokia.com>
2024-08-13 15:55:54 +0530

    Push simple  base linked list code to create and print singly linked list

---
+++ linked_list/simple_base_linked_list.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+ 
+# Creating the list
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
