f91c521ded66f78f49d33760f3a88170fc49b82d
Naga Puneeth Devatha <devatha.naga_puneeth@nokia.com>
2024-08-13 16:09:50 +0530

    Add print linked list function to simple base

--- linked_list/simple_base_linked_list.py
+++ linked_list/simple_base_linked_list.py
@@ -8,3 +8,16 @@ head = Node(1)
 head.next = Node(2)
 head.next.next = Node(3)
 head.next.next.next = Node(4)
+
+
+def print_linked_list():
+    current_node = head
+    while True:
+        if current_node is None:
+            break
+        else:
+            print(current_node.value, end=' ')
+            current_node = current_node.next
+    print()
+
+print_linked_list()
